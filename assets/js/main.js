22b47af16720b97a133d8b1ecc4f11bb1252805d
Jazel Lim <lim.jazel@gmail.com>
2023-07-24 19:23:39 +0800

    Add navigation toggle for small screens

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+let nav = document.getElementById("nav-sm")
+let navWrapper = document.getElementById("nav-wrapper")
+let navBackdrop = document.getElementById("nav-backdrop")
+let openNavButton = document.getElementById("open-nav")
+let closeNavButton = document.getElementById("close-nav")
+
+function openNav() {
+	// show navigation
+	if (nav.classList.contains("hidden")) {
+		nav.classList.remove("hidden")
+		nav.classList.add("block")
+
+		navWrapper.classList.add("w-full")
+
+		navBackdrop.classList.remove("hidden")
+		navBackdrop.classList.add("block")
+
+		openNavButton.classList.remove("block")
+		openNavButton.classList.add("hidden")
+
+		closeNavButton.classList.remove("hidden")
+		closeNavButton.classList.add("block")
+	}
+	// hide navigation
+	else if (nav.classList.contains("block")) {
+		closeNav()
+	}
+}
+
+function closeNav() {
+	nav.classList.remove("block")
+	nav.classList.add("hidden")
+
+	navWrapper.classList.remove("w-full")
+
+	navBackdrop.classList.remove("block")
+	navBackdrop.classList.add("hidden")
+
+	openNavButton.classList.remove("hidden")
+	openNavButton.classList.add("block")
+
+	closeNavButton.classList.remove("block")
+	closeNavButton.classList.add("hidden")
+}
