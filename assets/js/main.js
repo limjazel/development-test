77cee1b644160d7567a8aeb7985ed6b509569213
Jazel Lim <lim.jazel@gmail.com>
2023-07-25 00:17:26 +0800

    Add styling to navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,35 @@
-/* 
-Swiper.js (Carousell/Slides) configuration 
-https://swiperjs.com/swiper-api#initialize-swiper 
+/*
+Swiper.js (Carousell/Slides) configuration
+https://swiperjs.com/swiper-api#initialize-swiper
 */
 const swiper = new Swiper(".swiper", {
-	a11y: {
-		prevSlideMessage: "Previous slide",
-		nextSlideMessage: "Next slide",
-	},
+  a11y: {
+    prevSlideMessage: "Previous slide",
+    nextSlideMessage: "Next slide",
+  },
 
-	// Optional parameters
-	direction: "horizontal",
-	loop: true,
-	slidesPerView: 3,
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 3,
 
-	// If we need pagination
-	pagination: {
-		el: ".swiper-pagination",
-		dynamicBullets: true,
-	},
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
 
-	// Navigation arrows
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
-	// And if we need scrollbar
-	scrollbar: {
-		el: ".swiper-scrollbar",
-		hide: true,
-	},
-})
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+});
 
 /* End of Swiper.js (Carousell/Slides) configuration */
--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,44 +1,71 @@
-let nav = document.getElementById("nav-sm")
-let navWrapper = document.getElementById("nav-wrapper")
-let navBackdrop = document.getElementById("nav-backdrop")
-let openNavButton = document.getElementById("open-nav")
-let closeNavButton = document.getElementById("close-nav")
+let nav = document.getElementById("nav-sm");
+let navWrapper = document.getElementById("nav-wrapper");
+let navBackdrop = document.getElementById("nav-backdrop");
+let openNavButton = document.getElementById("open-nav");
+let closeNavButton = document.getElementById("close-nav");
 
 function openNav() {
-	// show navigation
-	if (nav.classList.contains("hidden")) {
-		nav.classList.remove("hidden")
-		nav.classList.add("block")
+  // show navigation
+  if (nav.classList.contains("hidden")) {
+    nav.classList.remove("hidden");
+    nav.classList.add("block");
 
-		navWrapper.classList.add("w-full")
+    navWrapper.classList.add("w-full");
 
-		navBackdrop.classList.remove("hidden")
-		navBackdrop.classList.add("block")
+    navBackdrop.classList.remove("hidden");
+    navBackdrop.classList.add("block");
 
-		openNavButton.classList.remove("block")
-		openNavButton.classList.add("hidden")
+    openNavButton.classList.remove("block");
+    openNavButton.classList.add("hidden");
 
-		closeNavButton.classList.remove("hidden")
-		closeNavButton.classList.add("block")
-	}
-	// hide navigation
-	else if (nav.classList.contains("block")) {
-		closeNav()
-	}
+    closeNavButton.classList.remove("hidden");
+    closeNavButton.classList.add("block");
+  }
+  // hide navigation
+  else if (nav.classList.contains("block")) {
+    closeNav();
+  }
 }
 
 function closeNav() {
-	nav.classList.remove("block")
-	nav.classList.add("hidden")
+  nav.classList.remove("block");
+  nav.classList.add("hidden");
 
-	navWrapper.classList.remove("w-full")
+  navWrapper.classList.remove("w-full");
 
-	navBackdrop.classList.remove("block")
-	navBackdrop.classList.add("hidden")
+  navBackdrop.classList.remove("block");
+  navBackdrop.classList.add("hidden");
 
-	openNavButton.classList.remove("hidden")
-	openNavButton.classList.add("block")
+  openNavButton.classList.remove("hidden");
+  openNavButton.classList.add("block");
 
-	closeNavButton.classList.remove("block")
-	closeNavButton.classList.add("hidden")
+  closeNavButton.classList.remove("block");
+  closeNavButton.classList.add("hidden");
 }
+
+/*
+Alpine.js Alpine.data configuration for navigation links
+https://alpinejs.dev/globals/alpine-data
+*/
+let navLinks = [
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "Features",
+    url: "/",
+  },
+  {
+    name: "Solutions",
+    url: "/",
+  },
+  {
+    name: "Resources",
+    url: "/",
+  },
+  {
+    name: "Pricing",
+    url: "/",
+  }
+];
