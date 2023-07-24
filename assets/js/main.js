7a42db16e0cb033f98f7159e3a2486a8e159ed02
Jazel Lim <lim.jazel@gmail.com>
2023-07-25 00:22:17 +0800

    Move slide data to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
+let slides = [
+  {
+    stars: 4.2,
+    rating: "4.2/5",
+    reviews: "45k",
+    photo_url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Trustpilot_Logo_%282022%29.svg/320px-Trustpilot_Logo_%282022%29.svg.png",
+    source: "Trustpilot",
+  },
+  {
+    stars: 4.1,
+    rating: "4.1/5",
+    reviews: "18k",
+    photo_url:
+      "https://capterra.s3.amazonaws.com/assets/images/logos/capterra.png",
+    source: "Capterra",
+  },
+];
+
 /*
 Swiper.js (Carousell/Slides) configuration
 https://swiperjs.com/swiper-api#initialize-swiper
